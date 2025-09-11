a2374ab2f6ebcde03a97ae7a25941db87337ffa2
Denzel Zimba <ST10383606@vconnect.edu.za>
2025-09-11 17:34:50 +0200

    Create Functioning API Endpoints for Registering and Loggin in Users with Firebase Authentication

--- Mobile/app/build.gradle.kts
+++ Mobile/app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Biometric Authentication library
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
